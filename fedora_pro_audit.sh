268459e2fabbf5610da8eb3bf7dbf7bf99700c06
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-12-24 20:26:02 +0330

    [Feat] : Initialize environment done

--- fedora_pro_audit.sh
+++ fedora_pro_audit.sh
@@ -11,3 +11,13 @@
 LOG_DIR="/var/log/sys_audit"
 LOG_FILE="$LOG_DIR/audit_$(date +%Y%m%d).log"
 DISK_THRESHOLD=80
+
+# Initialize environment
+setup_env(){
+    if ([[EUID -ne 0$]]); then
+        echo "Error: This script must be run as root." >&2
+            exit 1
+    fi
+    mkdir -p $LOG_DIR
+    touch $LOG_FILE
+}
